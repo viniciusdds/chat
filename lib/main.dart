eca0618ba540fc6d93dafe322168bf081414b251
viniciusdds <viniciusdamasceno180@hotmail.com>
2021-09-13 20:01:11 -0300

    aula 382

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chat/pages/auth_page.dart';
+import 'package:chat/pages/auth_or_app_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AuthPage(),
+      home: AuthOrAppPage(),
       debugShowCheckedModeBanner: false,
     );
   }
